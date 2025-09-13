0c92d58047896253f5b1fdbabb90524e5d4548f8
MidoGad <ahmedmohamedgad2511@gmail.com>
2025-09-13 18:18:36 +0300

    Update session4.py
    case 2

--- session4.py
+++ session4.py
@@ -11,3 +11,5 @@ match inpute:
         num1=int(input("enter the first number : "))
         num2=int(input("enter the sec number : "))
         print(f"the sum of {num1} and {num2} is {num1 + num2}")
+    case "2":
+        print("random num : "+radndom.randint(0,100))
